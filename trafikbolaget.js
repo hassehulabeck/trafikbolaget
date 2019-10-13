a32d2fa9d0f7f9d61e39ae32b3ad94015bb75ad1
hassehulabeck <hansbudandersson@gmail.com>
2019-10-13 13:25:10 +0200

    Added good code

--- trafikbolaget.js
+++ trafikbolaget.js
@@ -17,3 +17,39 @@ stops.forEach(function (stop) {
 
 // Lägg till linje1 i arrayen som samlar alla linjer.
 linjer = [linje1];
+
+// Skapa bussen (den här gången utan passagerarfunktionerna)
+var bus = {
+    id: 1,
+    linje: 1,
+    driver: {
+        name: "Olle Blom",
+        age: 53
+    },
+    passengers: [],
+    position: null,
+    direction: 1,
+    isAtStop: false,
+    maxPassengers: 20,
+    emptySeats: function () {
+        return this.maxPassengers - this.passengers.length;
+    },
+    // En funktion för att flytta bussen ett steg.
+    move: function () {
+        // Kolla först om vi är i någon av ändarna, och vidtag i så fall åtgärder.
+        if ((this.position == 0) && (this.direction == -1))
+            this.direction = 1;
+        if ((this.position == linje1.length - 1) && (this.direction == 1))
+            this.direction = -1
+
+        // Flytta bussen genom att använda direction-värdet.
+        this.position = this.position + this.direction;
+
+        // Skriv ut var någonstans bussen är.
+        console.log(linje1[this.position].name);
+
+    }
+}
+
+// Placera bussen i en ände av linje1.
+bus.position = 0;
