ecbe60007512e44eed4fc69fbd2f04f45e824b07
hassehulabeck <hansbudandersson@gmail.com>
2019-10-18 12:43:44 +0200

    First commit

--- README.md
+++ README.md
@@ -2,17 +2,22 @@
 ## En genomgång av javaScript i kapitelform
 I varje kapitel berörs ett eller flera kunskapsområden. Skriv kod som löser uppgiften.
 ### Kapitel 1 - Chaufför och passagerare
-#### Kunskaper: Stränghantering och funktion
+#### Kunskaper: Stränghantering
 #### Uppgift: Skapa personer.
 Skapa en chaufför och tre passagerare. Chauffören och passagerarna ska ha för- och efternamn. Använd strängar, inte objekt för personerna!
-Skriv en funktion som returnerar förnamnet på den person som du skickar med som argument. 
+Skriv därefter kod som: 
+* Skriver ut (med console.log) förnamnet på chauffören
+* Skriver ut efternamnet på passagerare 1.
+* Skriver ut efternamnet på passagerare 2 i VERSALER.
+* Ändrar alla "e" eller "E" i namnet på passagerare 3 till "3".
+
+Se till att du lagrar värdet av de manipulerade strängarna i en ny variabel, och kontrollera dess innehåll genom att skriva namnet på den i webbläsarens console.
+
 #### Tekniker & metoder
 ```javascript
+variabler
 .indexOf
 .slice
-```
-
-Funktionsanropet ska se ut så här: 
-```javascript
-console.log(getFirstName(driver));
+.toUpper
+.replace
 ```
--- trafikbolaget.js
+++ trafikbolaget.js
@@ -4,17 +4,28 @@ var driver = "Irene Gustavsson";
 // Variabler för passagerarna
 var passenger1 = "Filip Jovetevic";
 var passenger2 = "Kim Hyeung";
-var passenger3 = "Amanda Ringskog";
+var passenger3 = "Evander Ringskog";
 
-// Skriv ut förnamnet
-function getFirstName(person) {
-    // Skapa en lokal variabel som ska innehålla personens förnamn.
-    let firstName;
-    // Och en variabel som lagrar positionen för mellanslaget i namnet.
-    let blankspace = person.indexOf(" ");
-    // Hämta sedan ut alla bokstäver från position 0 till mellanslaget.
-    firstName = person.slice(0, blankspace);
+// Skriv ut förnamnet på chauffören
 
-    // Till slut använder vi return för att skicka tillbaks värdet.
-    return firstName;
-}
+// Skapa en lokal variabel som ska innehålla personens förnamn.
+let firstName;
+
+// Och en variabel som lagrar positionen för mellanslaget i namnet.
+let blankspace = driver.indexOf(" ");
+
+// Hämta sedan ut alla bokstäver från position 0 till mellanslaget.
+firstName = driver.slice(0, blankspace);
+
+// Skriv ut efternamnet på passagerare 1
+let surnameP1 = passenger1.slice(passenger1.indexOf(" ") + 1, );
+
+// Skriv ut EFTERNAMNET på passagerare 2
+let surnameP2 = passenger2.slice(passenger2.indexOf(" ") + 1, ).toUpperCase();
+
+//Byt ut e och E till 3
+let eName = passenger3.replace("e", "3");
+eName = eName.replace("E", "3");
+
+// Alternativt, byt ut både stort och litet E med regex.
+let regexName = passenger3.replace(/[eE]/g, "3");
