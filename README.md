c832370f6ee9bb9daf9153dfa4cb6db0dfacba93
hassehulabeck <hansbudandersson@gmail.com>
2019-10-23 10:39:34 +0200

    Added DOMed line

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Trafikbolaget
 ## En genomgång av javaScript i kapitelform
 I varje kapitel berörs ett eller flera kunskapsområden. Skriv kod som löser uppgiften.
-### Kapitel 16 - Kolla biljetten mer noggrant
-#### Kunskaper: Date
-#### Uppgift: Kolla tiden på biljetten.
-Låt oss säga att en stämpling har en giltighet på 90 minuter. Då är det bra om vi kan baka in en sådan kolla när vi anropar stämplings-funktionen, så att vi inte stämplar i onödan.
+### Kapitel 17 - Visualisera bussen och linjen
+#### Kunskaper: DOM
+#### Uppgift: Rita upp bussen.
+Skapa i html-filen lämpliga element som kan representera hållplatser. Använd sedan DOM-manipulation för att rendera (rita ut) bussen.
+Lägg gärna bussens move-funktion inne i en loop som styrs av setInterval, så får du en kontinuerlig rörelse (om än hackig) på bussen.
 
 #### Tekniker & metoder
 ```javascript
-Date
+DOM
+setInterval
 ``` 
--- trafikbolaget.mjs
+++ trafikbolaget.mjs
@@ -1,28 +1,14 @@
 import {
-    passengers
-} from './modules/passengers.mjs';
-import {
-    ticketId,
-    stampTicket,
-    Ticket
-} from './modules/ticket.mjs';
+    lines,
+    linje1
+} from './modules/lines.mjs';
+import bus from './modules/bus.mjs'
 
-// Kolla hur många passagerare vi har
-console.log(passengers.length);
+// Använd DOM för att skapa hållplatser.
+var linjen = document.getElementById("linje1");
 
-// Skapa en biljett med 10 credit
-var ticket = new Ticket();
-
-// Kolla om biljetten är valid.
-console.log(ticket.isValid());
-
-// Gör passengers och ticket tillgängligt i console
-window.passengers = passengers;
-window.ticket = ticket;
-window.stampTicket = stampTicket;
-
-// Tilldela en ticket till en passagerare.
-passengers[0].ticket = ticket;
-
-// Stämpla biljetten för samma passagerare
-stampTicket(passengers[0].ticket);
+linje1.forEach(stop => {
+    let newStop = document.createElement("li");
+    newStop.innerText = stop.name;
+    linjen.appendChild(newStop);
+});
